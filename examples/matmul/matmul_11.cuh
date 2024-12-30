f2be135145ec49042423c2c66334f2b9c8906102
Aroun Demeure <arundemeure@gmail.com>
2024-12-30 21:43:12 +0100

    Update matmul_11.cuh
    Siiiigh

--- examples/matmul/matmul_11.cuh
+++ examples/matmul/matmul_11.cuh
@@ -432,8 +432,7 @@ __global__  __launch_bounds__(NUM_THREADS) void  __cluster_dims__(CLUSTER_M * CL
         int p = 0;
         int qidx = 0;
         int num_block_m, num_block_n;
-				bool schedule_next = schedule.next(num_block_m, num_block_n);
-        while (schedule_next) {
+        while (schedule.next(num_block_m, num_block_n);) {
             num_block_n = num_block_n * CLUSTER_N + rank_n;
             num_block_m = num_block_m * CLUSTER_M + rank_m;
             {
@@ -515,8 +514,6 @@ __global__  __launch_bounds__(NUM_THREADS) void  __cluster_dims__(CLUSTER_M * CL
  }
 
 asm volatile("bar.sync 1, 256;\n");
-
-schedule_next = schedule.next(num_block_m, num_block_n);
 
 if (true) {
 
